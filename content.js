42e7ae6c43243cba5a13be8bf0bba632f20d9762
陆奕玮 <849544677@qq.com>
2026-09-19 17:03:50 +0800

    Update content.js

--- content.js
+++ content.js
@@ -32,9 +32,9 @@ const portfolio = {
     { category: "automation", number: "13", title: "抖音点赞工具", description: "使用python自动化完成的抖音点赞工具，能完成自动点赞自动收藏等操作，可以批量完成。", tags: ["python", "Drissionpage", "xpath"], link: "Portfolio/抖音点赞.html", accent: "mint", bgImage: "Portfolio/抖音点赞工具.png" },
     { category: "automation", number: "14", title: "多平台价格监控", description: "使用python脚本完成对各商城数据商品的监控，达到目标价格进行微信提醒。", tags: ["python", "Drissionpage", "xpath"], link: "#", accent: "mint", bgImage: "Portfolio/多平台价格监控.png" },
     { category: "creative", number: "15", title: "文章搭建模版", description: "各种热门类型文章的搭建模版，包括但不限于技术博客、学习路线、技术介绍等。", tags: ["csdn", "文章模版"], link: "Portfolio/个人文章搭建模版.html", accent: "mint", bgImage: "Portfolio/bgimgs/csdn.png"},
-    { category: "crawler", number: "16", title: "贝壳二手房数据采集", description: "爬虫采集贝壳二手房数据，保存Excel文件", tags: ["requests", "xpath","反爬策略"], link: "Portfolio/贝壳二手房/index.html", accent: "mint", bgImage: "Portfolio/bgimgs/贝壳二手房.png"},
+    { category: "crawler", number: "16", title: "贝壳二手房数据采集", description: "爬虫采集贝壳二手房数据，保存Excel文件", tags: ["requests", "xpath","反爬策略"], link: "Portfolio/贝壳数据采集.html", accent: "mint", bgImage: "Portfolio/bgimgs/贝壳二手房.png"},
     { category: "analysis", number: "17", title: "csv自动化大屏", description: "csv数据可视化大屏，支持数据筛选、图表切换。", tags: ["pandas", "pyecharts","Excel"], link: "Portfolio/csv自动化大屏/CSV自动大屏.html", accent: "coral", bgImage: "Portfolio/bgimgs/数据大屏.png"},
-    { category: "creative", number: "18", title: "炫酷项目展示", description: "AIGC技术项目展示，展示AIGC技术项目，包括但不限于AI绘画、AI写作、AI编程等。", tags: ["AIGC", "提示词","python"], link: "Portfolio/炫酷项目展示.html", accent: "coral", bgImage: "Portfolio/bgimgs/AIGC.png"},
+    { category: "creative", number: "18", title: "炫酷项目展示：康康（AI版）", description: "AIGC技术项目展示，展示AIGC技术项目，包括但不限于AI绘画、AI写作、AI编程等。", tags: ["AIGC", "提示词","python"], link: "Portfolio/炫酷项目展示.html", accent: "coral", bgImage: "Portfolio/bgimgs/zmjjkk.png"},
      { category: "creative", number: "19", title: "康神开播了？", description: "都是同龄人我原本没想降维打击", tags: ["vctcn的王", "绝对的狙神","豪"], link: "Portfolio/郑永康.html", accent: "coral", bgImage: "Portfolio/bgimgs/zmjjkk.png"}
   ]
 };
